0bc6344c23df0cb84ef13127131f09c13a2c5aa5
Chris Kinsman <chrisknsmn@gmail.com>
2025-05-14 11:21:23 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <>
-      <main
-        className="h-screen w-screen flex justify-center items-center bg-cover bg-center"
-        style={{ backgroundImage: "url('/images/banner-image.jpg')" }}
-      >
-        <section>
-          <Image
-            src="/images/bnr-logo.png"
-            alt="Arpinomoda Logo"
-            width={834}
-            height={334}
-            className="max-w-[300px]"
-          />
-        </section>
-      </main>
-    </>
+    <main
+      className="h-screen w-screen flex justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/banner-image.jpg')" }}
+    >
+      <section>
+        <Image
+          src="/images/bnr-logo.png"
+          alt="Arpinomoda Logo"
+          width={834}
+          height={334}
+          className="max-w-[300px]"
+        />
+      </section>
+    </main>
   );
 }
